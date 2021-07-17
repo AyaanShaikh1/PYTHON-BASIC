caee7962a836311be67e5a6027d66d042e608e33
AyaanShaikh1 <70950484+AyaanShaikh1@users.noreply.github.com>
2021-07-17 20:57:38 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,13 @@
+name = input('Enter your Name: ')
+print('Hello ' + name)
+
+if name == "Ayaan" : 
+    print('I know you ')
+else : 
+    print('I don`t know you') 
+
+numbers=[1,2,3,4,5,6,7,8,9,10]
+len(numbers)
+print(numbers[-9])
+
+for number in numbers : print(number) 
